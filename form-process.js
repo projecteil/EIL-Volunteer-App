b44dcb42b3aaaeb6571b83856e9af12b0cddc72f
anshyadav <yadavansh04@gmail.com>
2021-03-01 23:20:25 +0000

    14

--- form-process.js
+++ form-process.js
@@ -25,7 +25,10 @@ function processForm() {
     fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
             method: "POST",
             headers: {
-                "Content-type": "application/json;charset=UTF-8"
+                "Content-type": "application/json;charset=UTF-8",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
             },
         }).then(json => console.log(json)).catch((error) => {
             console.error('Error:', error);
