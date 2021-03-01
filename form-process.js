b5b19dc62f44a9ccec58bb1266478aafa3c4c806
anshyadav <yadavansh04@gmail.com>
2021-03-01 23:08:02 +0000

    13

--- form-process.js
+++ form-process.js
@@ -23,7 +23,6 @@ function processForm() {
 
 
     fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
-            mode: "no-cors",
             method: "POST",
             headers: {
                 "Content-type": "application/json;charset=UTF-8"
