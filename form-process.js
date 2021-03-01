ab0723d36b474fbf4e2619b2df8f88aff1ab1df2
anshyadav <yadavansh04@gmail.com>
2021-03-01 19:38:52 +0000

    first commit

---
+++ form-process.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    document.getElementById("ContactPreference").addEventListener("focus", processForm);
+}
+
+function processForm() {
+    let firstName = document.getElementById("FirstName").value;
+    let lastName = document.getElementById("LastName").value;
+    let phoneNumber = document.getElementById("PhoneNumber").value;
+    let email = document.getElementById("Email").value;
+    let password = document.getElementById("Password").value;
+    let bio = document.getElementById("Bio").value;
+    let skills = document.getElementById("Skills").value;
+    let dateOfBirth = document.getElementById("DateOfBirth").value;
+    let contactPreference = document.getElementById("ContactPreference").value;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(phoneNumber);
+    console.log(email);
+    console.log(password);
+    console.log(bio);
+    fetch('https://eilireland.my.salesforce.com/services/data/v25.0/sobjects/Contact/0036N000005PKBcQAO', {
+            mode: 'no-cors',
+            method: "GET",
+            headers: {
+                mode: 'no-cors',
+                "Content-type": "application/json;charset=UTF-8",
+                "Authorization": "Bearer 00D1t000000DDG9!AR0AQMufdpHAJWCxV.wq6TCsCeO1R3gE5AwSfKKiDvLp1703CRVUjIgOejYufcg8rYf40tVfOZth5kIncrKDruimW.2jVFIW"
+            }
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+}
