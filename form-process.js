6ab635d928044dcffb0d712a80612b4a3d61d7a1
anshyadav <yadavansh04@gmail.com>
2021-03-02 10:42:50 +0000

    21

--- form-process.js
+++ form-process.js
@@ -19,11 +19,10 @@ function processForm() {
     console.log(password);
     console.log(bio);
     let data ="";
-    let accessToken = "";
 
 
 
-    accessToken = fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
+    let accessToken = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
             method: "POST",
             headers: {
                 "Content-type": "application/json;charset=UTF-8",
@@ -31,9 +30,10 @@ function processForm() {
                 "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
                 "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
             },
-        }).then((res) => {data=res});
+        });
 
-        setTimeout(console.log(accessToken), 4000);
-        setTimeout(console.log(data), 5000);
+        let data = await response.json();
+        setTimeout(console.log(data), 8000);
+        setTimeout(console.log(accessToken),12000);
         
 }
