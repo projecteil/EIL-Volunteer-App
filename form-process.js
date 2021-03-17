7437f7eebd82aa6016e7684aa2826e4a9cbb7fcf
anshyadav <yadavansh04@gmail.com>
2021-03-17 21:17:04 +0000

    UI changes according to prototype

--- form-process.js
+++ form-process.js
@@ -1,13 +1,18 @@
 async function processForm() {
+
+    let formDiv = document.getElementById('msform');
+    let confirmEmailDiv = document.getElementById('emailconfirm');
     let firstName = document.getElementById("FirstName").value;
     let lastName = document.getElementById("LastName").value;
     let phoneNumber = document.getElementById("PhoneNumber").value;
     let email = document.getElementById("Email").value;
     let password = document.getElementById("Password").value;
-    let bio = document.getElementById("Bio").value;
-    let skills = document.getElementById("Skills").value;
     let dateOfBirth = document.getElementById("DateOfBirth").value;
-    let contactPreference = document.getElementById("ContactPreference").value;
+    
+    formDiv.style.cssText = "display: None";
+    confirmEmailDiv.style.cssText = "display: block !important"
+
+
 
 
     let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
--- script.js
+++ script.js
@@ -75,7 +75,3 @@ $(".previous").click(function(){
 		easing: 'easeInOutBack'
 	});
 });
-
-$(".submit").click(function(){
-	return false;
-})
