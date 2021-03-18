f534ebfde7fa8880d335defb3a8ef8d51a4ede2e
anshyadav <yadavansh04@gmail.com>
2021-03-18 03:47:32 +0000

    login page

--- form-process.js
+++ form-process.js
@@ -2,6 +2,7 @@ async function processForm() {
 
     let formDiv = document.getElementById('msform');
     let confirmEmailDiv = document.getElementById('emailconfirm');
+    let errorMsg = document.getElementById('errormsg');
     let firstName = document.getElementById("FirstName").value;
     let lastName = document.getElementById("LastName").value;
     let phoneNumber = document.getElementById("PhoneNumber").value;
@@ -9,12 +10,18 @@ async function processForm() {
     let password = document.getElementById("Password").value;
     let dateOfBirth = document.getElementById("DateOfBirth").value;
 
-    formDiv.style.cssText = "display: None";
-    confirmEmailDiv.style.cssText = "display: block !important"
-    dateOfBirth = dateOfBirth.split('/');
-    dateOfBirth = dateOfBirth[1]+"/"+dateOfBirth[0]+"/"+dateOfBirth[2];
-    dateOfBirth = new Date(dateOfBirth);
-    dateOfBirth = dateOfBirth.toISOString();
+    if (firstName == "") {
+        console.log("Empty");
+        errorMsg.innerHTML = "First Name can't be empty";
+    } else {
+        formDiv.style.cssText = "display: None";
+        confirmEmailDiv.style.cssText = "display: block !important"
+        dateOfBirth = dateOfBirth.split('/');
+        dateOfBirth = dateOfBirth[1] + "/" + dateOfBirth[0] + "/" + dateOfBirth[2];
+        dateOfBirth = new Date(dateOfBirth);
+        dateOfBirth = dateOfBirth.toISOString();
+    }
+
 
 
 
@@ -52,7 +59,7 @@ async function processForm() {
         "LastName": lastName,
         "MobilePhone": phoneNumber,
         "Email": email,
-        "Password__c": password,
+        "Passcode__c": password,
         "Birthdate": dateOfBirth,
         "AccountId": "0011t00000ppMtOAAU"
     }
@@ -68,4 +75,55 @@ async function processForm() {
         .then(response => response.json())
         .then(json => console.log(json))
         .catch(err => console.log(err));
+}
+
+async function processLogIn() {
+
+    let loader = document.getElementById('loader');
+    let formDiv = document.getElementById('msform');
+    let errorMsg = document.getElementById('errormsg');
+    let email = document.getElementById("Email").value;
+    let password = document.getElementById("Password").value;
+
+    if (email == "") {
+        console.log("Empty");
+        errorMsg.innerHTML = "Email can't be empty";
+    } else {
+        formDiv.style.cssText = "display:none;";
+        loader.style.cssText = "display:block;";
+        //window.location.replace("./dashboard-home.html");
+    }
+
+
+
+
+
+
+    let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json;charset=UTF-8",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+        },
+    });
+
+    let data = await response.json();
+    setTimeout(console.log(data), 8000);
+    setTimeout(console.log(data["access_token"]), 13000);
+
+
+
+    let responseViewContact = await fetch("https://eilireland.my.salesforce.com/services/data/v25.0/query?q=select+id+from+Contact+where+Email+=+'ansh31@gmail.com'", {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json;charset=UTF-8",
+                "Authorization": "Bearer " + data["access_token"]
+            }
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
+
 }
--- script.js
+++ script.js
@@ -1,77 +1,31 @@
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+//test for iterating over child elements
+var langArray = [];
+$('#onlinestatus option').each(function () {
+	var img = $(this).attr("data-thumbnail");
+	var text = this.innerText;
+	var value = $(this).val();
+	var item = '<li><img src="' + img + '" alt="" value="' + value + '"/><span>' + text + '</span></li>';
+	langArray.push(item);
+})
 
-$(".next").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({
-        'transform': 'scale('+scale+')',
-        'position': 'absolute'
-      });
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
+$('#a').html(langArray);
+
+//Set the button value to the first el of the array
+$('.btn-select').html(langArray[0]);
+$('.btn-select').attr('value', 'en');
+
+//change button stuff on click
+$('#a li').click(function () {
+	var img = $(this).find('img').attr("src");
+	var value = $(this).find('img').attr('value');
+	var text = this.innerText;
+	var item = '<li><img src="' + img + '" alt="" /><span>' + text + '</span></li>';
+	$('.btn-select').html(item);
+	$('.btn-select').attr('value', value);
+	$(".b").toggle();
+	//console.log(value);
 });
 
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
+$(".btn-select").click(function () {
+	$(".b").toggle();
 });
