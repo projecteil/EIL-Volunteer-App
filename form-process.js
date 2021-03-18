422aa5055822465c22e8815e6448ba3cfe4bc835
anshyadav <yadavansh04@gmail.com>
2021-03-18 12:56:46 +0000

    V1 prototype

--- form-process.js
+++ form-process.js
@@ -3,6 +3,7 @@ async function processForm() {
     let formDiv = document.getElementById('msform');
     let confirmEmailDiv = document.getElementById('emailconfirm');
     let errorMsg = document.getElementById('errormsg');
+    let errorIcon = document.getElementById('erroricon');
     let firstName = document.getElementById("FirstName").value;
     let lastName = document.getElementById("LastName").value;
     let phoneNumber = document.getElementById("PhoneNumber").value;
@@ -71,10 +72,15 @@ async function processLogIn() {
     let errorMsg = document.getElementById('errormsg');
     let email = document.getElementById("Email").value;
     let password = document.getElementById("Password").value;
+    const emailValidator = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (email == "") {
         console.log("Empty");
         errorMsg.innerHTML = "Email can't be empty";
+    } else if (!emailValidator.test(String(email).toLowerCase())) {
+        errorMsg.innerHTML = "Please enter the correct email ID";
+    } else if (password == "") {
+        errorMsg.innerHTML = "Password can't be empty";
     } else {
 
         let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
