00913b8697b9610e3dcaed4a69dd526860971577
anshyadav <yadavansh04@gmail.com>
2021-04-01 19:17:26 +0100

    8

--- form-process.js
+++ form-process.js
@@ -1,3 +1,5 @@
+let globalVar = "I am Global.";
+
 async function processForm() {
 
     let formDiv = document.getElementById('msform');
@@ -121,55 +123,39 @@ async function processLogIn() {
 
 async function editProfile() {
 
-    let loader = document.getElementById('loader');
-    let formDiv = document.getElementById('msform');
-    let errorMsg = document.getElementById('errormsg');
-    let email = document.getElementById("Email").value;
-    let password = document.getElementById("Password").value;
-    const emailValidator = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    console.log(globalVar);
+    // let loader = document.getElementById('loader');
+    // let formDiv = document.getElementById('msform');
+    // let errorMsg = document.getElementById('errormsg');
+    // let email = document.getElementById("Email").value;
+    // let password = document.getElementById("Password").value;
+    // const emailValidator = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (email == "") {
-        console.log("Empty");
-        errorMsg.innerHTML = "Email can't be empty";
-    } else if (!emailValidator.test(String(email).toLowerCase())) {
-        errorMsg.innerHTML = "Please enter the correct email ID";
-    } else if (password == "") {
-        errorMsg.innerHTML = "Password can't be empty";
-    } else {
+    let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json;charset=UTF-8",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+        },
+    });
 
-        let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json;charset=UTF-8",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-                "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-            },
-        });
+    let data = await response.json();
 
-        let data = await response.json();
-        let accessToken =
-
-            let responseViewContact = await fetch("https://eilireland.my.salesforce.com/services/data/v25.0/query?q=select+Passcode__c+from+Contact+where+Email+=+'" + email + "'", {
-                method: "GET",
-                headers: {
-                    "Content-type": "application/json;charset=UTF-8",
-                    "Authorization": "Bearer " + data["access_token"]
-                }
-            });
-
-        secretData = await responseViewContact.json();
-        setTimeout(console.log(secretData), 8000);
-        setTimeout(console.log(secretData["records"]["0"]["Passcode__c"]), 13000);
-        let serverPassword = secretData["records"]["0"]["Passcode__c"];
-
-        if (password == serverPassword) {
-            formDiv.style.cssText = "display:none;";
-            loader.style.cssText = "display:block;";
-            console.log("About to redirect.");
-            window.location.replace("./dashboard-home.html");
-
+    let responseGetProfile = await fetch("https://eilireland.my.salesforce.com/services/data/v25.0/query?q=Select+Name,Birthdate,Description,Passcode__c,Email,MobilePhone,HasOptedOutOfEmail,DoNotCall,npsp__Do_Not_Contact__c,MailingAddress,GW_Volunteers__Volunteer_Skills__c,GW_Volunteers__Volunteer_Availability__c,GW_Volunteers__Volunteer_Notes__c+FROM+Contact+Where+Email+=+'ansh45@gmail.com'", {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json;charset=UTF-8",
+            "Authorization": "Bearer " + data["access_token"]
         }
-    }
+    });
+
+    secretData1 = await responseViewContact.json();
+    setTimeout(console.log(secretData1), 8000);
+    // setTimeout(console.log(secretData["records"]["0"]["Passcode__c"]), 13000);
+    // let serverPassword = secretData["records"]["0"]["Passcode__c"];
+
+
 
 }
