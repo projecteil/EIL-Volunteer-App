7d733d88273bc3f83415c62215fb76d553313692
anshyadav <yadavansh04@gmail.com>
2021-04-05 21:48:05 +0100

    Commit from terminal. Experimental commit.

--- js/form-process.js
+++ js/form-process.js
@@ -1,6 +1,26 @@
-let globalVar = "I am Global.";
+var jsforce = require('jsforce');
 
 async function processForm() {
+
+
+    var conn = new jsforce.Connection({
+        // you can change loginUrl to connect to sandbox or prerelease env.
+        loginUrl: 'https://login.salesforce.com'
+    });
+    conn.login('project2@eilireland.org', 'OldMonk1234', function (err, userInfo) {
+        if (err) {
+            return console.error(err);
+        }
+        // Now you can get the access token and instance URL information.
+        // Save them to establish connection next time.
+        console.log(conn.accessToken);
+        console.log(conn.instanceUrl);
+        // logged in user property
+        console.log("User ID: " + userInfo.id);
+        console.log("Org ID: " + userInfo.organizationId);
+        // ...
+    });
+
 
     let formDiv = document.getElementById('msform');
     let confirmEmailDiv = document.getElementById('emailconfirm');
