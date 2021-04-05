d2a60970068e96730bb92fac60a8de213ec03faa
anshyadav <yadavansh04@gmail.com>
2021-04-05 20:12:27 +0100

    Commit from terminal. Experimental commit.

--- js/form-process.js
+++ js/form-process.js
@@ -32,7 +32,7 @@ async function processForm() {
 
     let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=OldMonk1234auRJQemePs9mac0guNA7ZrFa", {
         method: "POST",
-        mode: 'no-cors', // no-cors, *cors, same-origin
+        mode: '*cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         credentials: 'same-origin',
         headers: {
@@ -42,7 +42,6 @@ async function processForm() {
             "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
         },
     });
-
     let data = await response.json();
     setTimeout(console.log(data), 8000);
     setTimeout(console.log(data["access_token"]), 13000);
