d214169c8cba08c065ab145c00b916584fe7ef40
anshyadav <yadavansh04@gmail.com>
2021-06-13 17:49:04 +0100

    Commit from terminal. Experimental commit.

--- js/dashboard-home.js
+++ js/dashboard-home.js
@@ -13,9 +13,6 @@ function getCurrentDate() {
 
 async function processLogIn() {
     let currentDate = getCurrentDate();
-    console.log(currentDate);
-    let loader = document.getElementById('loader');
-
     let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=Secureit123AYfrE3tYJC7OVZtTEg0hgDkI", {
         method: "POST",
         mode: 'cors',
@@ -28,7 +25,6 @@ async function processLogIn() {
     });
 
     let data = await response.json();
-    console.log(data["access_token"]);
     let campaignData = await fetch("https://eilireland.my.salesforce.com/services/data/v25.0/query?q=select+Name,Type,StartDate,GW_Volunteers__Volunteers_Still_Needed__c,GW_Volunteers__Volunteer_Jobs__c+from+Campaign+Where+IsActive+=+True+And+EndDate+>=" + currentDate, {
         method: "GET",
         mode: 'cors',
@@ -38,9 +34,10 @@ async function processLogIn() {
         }
     });
 
-    secretData = await campaignData.json();
-    setTimeout(console.log(secretData), 8000);
-    setTimeout(console.log(secretData["records"]["0"]["Passcode__c"]), 13000);
+    campaignResponse = await campaignData.json();
+    console.log(campaignResponse);
+    console.log(campaignResponse.length);
+    setTimeout(console.log(campaignResponse["records"]["0"]["Passcode__c"]), 13000);
     let serverPassword = secretData["records"]["0"]["Passcode__c"];
 
     if (password == serverPassword) {
