5d242786d2b4ede5a1cb570f29ad533b51815fdd
anshyadav <yadavansh04@gmail.com>
2021-06-13 17:58:07 +0100

    Commit from terminal. Experimental commit.

--- js/dashboard-home.js
+++ js/dashboard-home.js
@@ -36,6 +36,7 @@ async function processLogIn() {
 
     campaignResponse = await campaignData.json();
     console.log(campaignResponse);
+    console.log(Object.keys(campaignResponse).length);
     Object.entries(campaignResponse).forEach(
         ([key, value]) => {
             if (key == "records") {
