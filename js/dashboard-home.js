2e2809c1b9b07bec9b9badf5e1a27d5d3d37bf1b
anshyadav <yadavansh04@gmail.com>
2021-06-13 21:14:43 +0100

    Commit from terminal. Experimental commit.

--- js/dashboard-home.js
+++ js/dashboard-home.js
@@ -1,5 +1,25 @@
 window.onload = function () {
     processLogIn();
+    if (getCookie("IsActive") == null || getCookie("IsActive") == " ") {
+        console.log("Cookie Not Present");
+    }
+
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 function getCurrentDate() {
