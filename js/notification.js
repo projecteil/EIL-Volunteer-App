48321a19e79492d0348db49a70ff8bcaed68070f
anshyadav <yadavansh04@gmail.com>
2021-07-02 15:20:29 +0100

    Commit from terminal. Experimental commit.

--- js/notification.js
+++ js/notification.js
@@ -14,22 +14,6 @@ function getCurrentDate() {
     return currentDate;
 }
 
-async function getToken() {
-    let response = await fetch("https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=3MVG9fTLmJ60pJ5LcM88X.T4cnlgFI6sTtiU0_tQwwMuyjIocVl289zYxysWrm45Y9JSHF0f55z.1SJoYFpkQ&client_secret=E2D30FFD226F098FDC26D1A0FA58581717B97678E30559C77F55C092B7899361&username=project2@eilireland.org&password=Secureit123AYfrE3tYJC7OVZtTEg0hgDkI", {
-        method: "POST",
-        mode: 'cors',
-        headers: {
-            "Content-type": "application/json;charset=UTF-8",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-        },
-    });
-
-    let data = await response.json();
-    return await data["access_token"];
-}
-
 async function getVolunteerStats() {
     let vArray = [];
     let realActiveCount = 0;
