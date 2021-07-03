c12900b62fcfa059d8d8808cb7266d3e6d394ce8
anshyadav <yadavansh04@gmail.com>
2021-07-03 13:41:31 +0100

    Commit from terminal. Experimental commit.

--- js/notification.js
+++ js/notification.js
@@ -58,7 +58,7 @@ async function loadNotifications() {
         for (let i = 0; i < noOfEmails; i++) {
             setTimeout(function () {
                 document.getElementById("ntfc").innerHTML += '<div class="notification-li"><div class="notification-image"><img src="img/emailnt.svg" ></div><div class="notification-text">' + taskObject["records"][i]["Subject"].substring(7) + '<br><span class="notification-date">Received on ' + taskObject["records"][i]["ActivityDate"] + '</span></div></div>';
-            }, i * 500);
+            }, i * 200);
 
         }
     } else {
